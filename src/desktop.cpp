38c06cd978e0f9f1b711b3358e1bcd6cdf701fd4
abVshel <abrashavosanshel@gmail.com>
2026-06-23 00:03:23 +0700

    feat: add desktop wallpaper and icons

--- src/desktop.cpp
+++ src/desktop.cpp
@@ -3,6 +3,34 @@
 
 void drawDesktop()
 {
-    setfillstyle(SOLID_FILL, BLUE);
-    bar(0, 0, getmaxx(), getmaxy());
+    readimagefile("../assets/wallpaper.bmp",
+                  0, 0,
+                  getmaxx(), getmaxy());
+
+    setcolor(WHITE);
+
+    readimagefile("../assets/browser.bmp",
+                  20, 20,
+                  80, 80);
+    outtextxy(25, 90, "Browser");
+
+    readimagefile("../assets/folder.bmp",
+                  20, 130,
+                  80, 190);
+    outtextxy(30, 200, "Folder");
+
+    readimagefile("../assets/settings.bmp",
+                  20, 240,
+                  80, 300);
+    outtextxy(25, 310, "Settings");
+
+    readimagefile("../assets/profile.bmp",
+                  20, 350,
+                  80, 410);
+    outtextxy(30, 420, "Profile");
+
+    readimagefile("../assets/power.bmp",
+                  20, 460,
+                  80, 520);
+    outtextxy(35, 530, "Power");
 }
