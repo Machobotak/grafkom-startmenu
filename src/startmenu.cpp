c491b3c6926e5938fd9a6cb66f3b1ae75ae7bdbb
R45FIQ <saiarifqi@gmail.com>
2026-06-22 22:06:14 +0700

    Update start menu style to Windows XP

--- src/startmenu.cpp
+++ src/startmenu.cpp
@@ -1,27 +1,156 @@
 #include <graphics.h>
+#include <math.h>
 #include "../include/startmenu.h"
 #include "../include/utils.h"
 
+static float animProgress = 0.0f;
+static float popupAnimProgress = 0.0f;
+static bool isHoveringPrograms = false;
+
 void drawStartMenu()
 {
-    if(!startMenuOpen)
-        return;
+    // Update progress animasi start menu
+    float animSpeed = 0.1f;
+    if (startMenuOpen) {
+        if (animProgress < 1.0f) animProgress += animSpeed;
+        if (animProgress > 1.0f) animProgress = 1.0f;
+    } else {
+        if (animProgress > 0.0f) animProgress -= animSpeed;
+        if (animProgress < 0.0f) animProgress = 0.0f;
+        isHoveringPrograms = false; // Reset state saat menu tertutup
+    }
 
-    setfillstyle(SOLID_FILL, LIGHTGRAY);
+    if (animProgress <= 0.0f) return;
 
-    bar(
-        0,
-        getmaxy() - 350,
-        250,
-        getmaxy() - 50
-    );
+    int mx = mousex();
+    int my = mousey();
 
-    setcolor(BLACK);
+    int startY = getmaxy() - 50; // Tinggi taskbar
+    int targetHeight = 450;
+    int currentHeight = (int)(targetHeight * animProgress);
 
-    outtextxy(20, getmaxy() - 320, "Browser");
-    outtextxy(20, getmaxy() - 280, "Settings");
-    outtextxy(20, getmaxy() - 240, "Profile");
-    outtextxy(20, getmaxy() - 200, "Shutdown");
+    int menuY = startY - currentHeight;
+    int menuX = 0;
+    int menuWidth = 350;
+
+    int leftColWidth = 200;
+    int rightColWidth = menuWidth - leftColWidth;
+    int headerHeight = 60;
+    int footerHeight = 45;
+
+    // Header (Blue - gaya XP)
+    setfillstyle(SOLID_FILL, BLUE);
+    bar(menuX, menuY, menuX + menuWidth, menuY + headerHeight);
+    
+    if (animProgress > 0.2f) {
+        setcolor(WHITE);
+        settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 2);
+        outtextxy(menuX + 60, menuY + 20, "Administrator");
+        
+        // Avatar placeholder
+        setfillstyle(SOLID_FILL, LIGHTGRAY);
+        bar(menuX + 10, menuY + 10, menuX + 50, menuY + 50);
+    }
+
+    // Left Column (White)
+    setfillstyle(SOLID_FILL, WHITE);
+    bar(menuX, menuY + headerHeight, menuX + leftColWidth, menuY + currentHeight - footerHeight);
+
+    // Right Column (Light Blue)
+    setfillstyle(SOLID_FILL, LIGHTBLUE);
+    bar(menuX + leftColWidth, menuY + headerHeight, menuX + menuWidth, menuY + currentHeight - footerHeight);
+
+    // Footer (Blue)
+    setfillstyle(SOLID_FILL, BLUE);
+    bar(menuX, menuY + currentHeight - footerHeight, menuX + menuWidth, menuY + currentHeight);
+
+    settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 1);
+    if (animProgress > 0.8f) {
+        // Teks Footer
+        setcolor(WHITE);
+        outtextxy(menuX + 20, menuY + currentHeight - 30, "Log Off");
+        outtextxy(menuX + 180, menuY + currentHeight - 30, "Turn Off Computer");
+
+        // Menu Kiri
+        setcolor(BLACK);
+        outtextxy(menuX + 15, menuY + headerHeight + 20, "Internet Explorer");
+        outtextxy(menuX + 15, menuY + headerHeight + 50, "Outlook Express");
+        outtextxy(menuX + 15, menuY + headerHeight + 80, "Windows Media Player");
+        outtextxy(menuX + 15, menuY + headerHeight + 110, "MSN Explorer");
+        outtextxy(menuX + 15, menuY + headerHeight + 140, "Windows Movie Maker");
+
+        // Pembatas
+        setcolor(LIGHTGRAY);
+        line(menuX + 10, menuY + headerHeight + 170, menuX + leftColWidth - 10, menuY + headerHeight + 170);
+
+        // Tombol All Programs
+        int allProgY = menuY + currentHeight - footerHeight - 30;
+        setcolor(BLACK);
+        outtextxy(menuX + 15, allProgY, "All Programs     >");
+
+        // Deteksi hover pada tombol "All Programs" atau pop-up menu
+        if (mx >= menuX && mx <= menuX + menuWidth + 200 && my >= allProgY - 20 && my <= startY) {
+            isHoveringPrograms = true;
+        } else {
+            isHoveringPrograms = false;
+        }
+
+        // Menu Kanan
+        setcolor(WHITE);
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 20, "My Documents");
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 50, "My Recent Documents");
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 80, "My Pictures");
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 110, "My Music");
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 140, "My Computer");
+        
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 180, "Control Panel");
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 210, "Connect To");
+        outtextxy(menuX + leftColWidth + 15, menuY + headerHeight + 240, "Printers and Faxes");
+    }
+
+    // Animasi Pop-up menu (All Programs)
+    if (isHoveringPrograms) {
+        if (popupAnimProgress < 1.0f) popupAnimProgress += animSpeed * 1.5f; // Pop-up lebih cepat
+        if (popupAnimProgress > 1.0f) popupAnimProgress = 1.0f;
+    } else {
+        if (popupAnimProgress > 0.0f) popupAnimProgress -= animSpeed * 1.5f;
+        if (popupAnimProgress < 0.0f) popupAnimProgress = 0.0f;
+    }
+
+    // Draw Pop-up menu
+    if (popupAnimProgress > 0.0f) {
+        int popupWidth = 180;
+        int popupTargetHeight = 250;
+        int popupHeight = (int)(popupTargetHeight * popupAnimProgress);
+        int popupX = menuX + leftColWidth; // Muncul dari batas kolom kiri
+        int popupY = menuY + currentHeight - footerHeight - popupTargetHeight;
+
+        // Shadow sederhana
+        setfillstyle(SOLID_FILL, DARKGRAY);
+        bar(popupX + 5, popupY + popupTargetHeight - popupHeight + 5, popupX + popupWidth + 5, popupY + popupTargetHeight + 5);
+
+        // Background pop-up
+        setfillstyle(SOLID_FILL, WHITE);
+        bar(popupX, popupY + popupTargetHeight - popupHeight, popupX + popupWidth, popupY + popupTargetHeight);
+        
+        // Border
+        setcolor(BLUE);
+        rectangle(popupX, popupY + popupTargetHeight - popupHeight, popupX + popupWidth, popupY + popupTargetHeight);
+
+        // Isi pop-up menu jika animasi hampir selesai
+        if (popupAnimProgress > 0.8f) {
+            setcolor(BLACK);
+            settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 1);
+            outtextxy(popupX + 15, popupY + 20, "Accessories");
+            outtextxy(popupX + 15, popupY + 50, "Games");
+            outtextxy(popupX + 15, popupY + 80, "Startup");
+            outtextxy(popupX + 15, popupY + 110, "Internet Explorer");
+            outtextxy(popupX + 15, popupY + 140, "MSN");
+            outtextxy(popupX + 15, popupY + 170, "Outlook Express");
+            outtextxy(popupX + 15, popupY + 200, "Remote Assistance");
+            outtextxy(popupX + 15, popupY + 230, "Windows Media Player");
+        }
+    }
 }
 
 void toggleStartMenu()
